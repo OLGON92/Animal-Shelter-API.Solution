b5b907b3676d9bb3511561cd1de7e629feab5b75
OLGON92 <ogon318@gmail.com>
2023-02-10 20:05:32 -0800

    update animal.cs file

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -2,12 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelterApi.Models
 {
-  public class AnimalShelterApi
+  public class Animal 
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "You must enter a comment!")]
     public string Name { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "You must enter a species!")]
     public string Species { get; set; }
+    [Required]
+    [StringLength(250, ErrorMessage = "You must enter description!")]
     public string Description { get; set; }
+    [Required(ErrorMessage = "Please enter an age.")]
     public int Age { get; set; }
 
     public DateTime DateAdded { get; set; }
